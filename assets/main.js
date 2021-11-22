93d28f4b93b42fc4d2fa265d5d8861749f1d33aa
sadberto <82291171+sadberto@users.noreply.github.com>
2021-11-22 23:32:55 +0100

    novo_web

---
+++ assets/main.js
@@ -0,0 +1,25 @@
+function menuResponsive() {
+    let menu = document.querySelector('.menu')
+    let resposive = document.querySelector('.menu-resposive')
+    let select = addEventListener('click' , e => {
+        const el = e.target;
+        if (el.classList.contains('burger') || el.classList.contains('menu-resposive')){
+            menu.classList.toggle('nav-active');
+        }
+        else {
+            menu.classList.remove('nav-active')
+        }
+        if (el.classList.contains('burger') || el.classList.contains('menu-resposive')) {
+            resposive.classList.toggle('toggle');
+        }
+        else {
+            resposive.classList.remove('toggle')
+        }
+        
+
+    });
+    
+        
+}
+
+menuResponsive();
