399bb44c97250ba2cd9c50311451dcfa9738a9b4
sadberto <82291171+sadberto@users.noreply.github.com>
2021-11-23 19:33:40 +0100

    new_miss

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,10 @@
 function menuResponsive() {
     let menu = document.querySelector('.menu')
     let resposive = document.querySelector('.menu-resposive')
-    let select = addEventListener('click' , e => {
+     addEventListener('click' , e => {
         const el = e.target;
         if (el.classList.contains('burger') || el.classList.contains('menu-resposive')){
-            alert('estou aqui');
-            menu.classList.add('nav-active');
-            
+            menu.classList.toggle('nav-active');
         }
         else {
             menu.classList.remove('nav-active')
