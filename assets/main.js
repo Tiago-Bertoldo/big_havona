e0b63e4cb98902544a349c0a76339a38705662b9
sadberto <82291171+sadberto@users.noreply.github.com>
2021-11-30 00:06:23 +0100

    new

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,12 @@
+function start() {
+    menuResponsive();
+    hoverProblems();
+}
+
+
 function menuResponsive() {
     let menu = document.querySelector('.menu')
+    let body = document.querySelector('.nav');
     let resposive = document.querySelector('.menu-resposive')
      addEventListener('click' , e => {
         const el = e.target;
@@ -22,4 +29,102 @@ function menuResponsive() {
         
 }
 
-menuResponsive();
+function hoverProblems(){
+    let icsRisk = document.querySelector('.risk');
+    let icsCost = document.querySelector('.cost');
+    let icsTransp = document.querySelector('.transparency');
+    let icsTrust = document.querySelector('.trust');
+    let icsPaper = document.querySelector('.paper');
+    let icsEmail = document.querySelector('.email');
+    let iconSelectd = document.querySelector('.ics_risk');
+    let iconSelectCost = document.querySelector('.ics_cost');
+    let iconSelectTransp = document.querySelector('.ics_transp');
+    let iconSelectTrust = document.querySelector('.ics_trust');
+    let iconSelectPaper = document.querySelector('.ics_paper');
+    let iconsSelectEmail = document.querySelector('.ics_email');
+    let sets = 0;
+    addEventListener('click' , e => {
+        const el = e.target ;
+        
+                if (el.classList.contains('ics_risk') && sets === 0 ){
+                    icsRisk.classList.toggle('trusts');
+                    iconSelectd.setAttribute('src' , 'img/minus.png')
+                    sets ++;
+                    console.log(sets)
+                }else {
+                if (el.classList.contains('ics_risk' || sets >= 1)) {
+                    console.log('estou aqui')
+                    icsRisk.classList.remove('trusts');
+                    iconSelectd.setAttribute('src' , 'img/plus.png')
+                    console.log(sets)
+                    sets = 0;
+                }
+            }
+                if (el.classList.contains('ics_cost') && sets === 0 ){
+                    icsCost.classList.toggle('trusts');
+                    iconSelectCost.setAttribute('src' , 'img/minus.png')
+                    sets ++;
+                }else {
+                if (el.classList.contains('ics_cost' || sets >= 1)) {
+                    icsCost.classList.remove('trusts');
+                    iconSelectCost.setAttribute('src' , 'img/plus.png')
+                    console.log(sets)
+                    sets = 0;
+                }
+            }
+
+                if (el.classList.contains('ics_transp') && sets === 0 ){
+                    icsTransp.classList.toggle('trusts');
+                    iconSelectTransp.setAttribute('src' , 'img/minus.png')
+                    console.log(sets)
+                    sets ++;
+                }else {
+                    if (el.classList.contains('ics_transp' || sets >= 1)) {
+                        icsTransp.classList.remove('trusts');
+                        iconSelectTransp.setAttribute('src' , 'img/plus.png')
+                        sets = 0;
+                    }
+                }
+        
+                if (el.classList.contains('ics_trust') && sets === 0 ){
+                    icsTrust.classList.toggle('trusts');
+                    iconSelectTrust.setAttribute('src' , 'img/minus.png')
+                    sets ++;
+                }else {
+                    if (el.classList.contains('ics_trust' || sets >= 1)) {
+                        icsTrust.classList.remove('trusts');
+                        iconSelectTrust.setAttribute('src' , 'img/plus.png')
+                        sets = 0;
+                    }
+                }
+        
+                if (el.classList.contains('ics_paper') && sets === 0 ){
+                    icsPaper.classList.toggle('trusts');
+                    iconSelectPaper.setAttribute('src' , 'img/minus.png')
+                    sets ++;
+                }else {
+                    if (el.classList.contains('ics_paper' || sets >= 1)) {
+                        icsPaper.classList.remove('trusts');
+                        iconSelectPaper.setAttribute('src' , 'img/plus.png')
+                        sets = 0;
+                    }
+                }
+       
+                if (el.classList.contains('ics_email') && sets === 0 ){
+                    icsEmail.classList.toggle('trusts');
+                    iconsSelectEmail.setAttribute('src' , 'img/minus.png')
+                    sets ++;
+                }else {
+                    if (el.classList.contains('ics_email' || sets >= 1)) {
+                        icsEmail.classList.remove('trusts');
+                        iconsSelectEmail.setAttribute('src' , 'img/plus.png')
+                        sets = 0;
+                    }
+                }
+
+                
+          
+    })
+}
+
+start();
