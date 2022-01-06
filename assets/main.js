0bad07f085d245f03df8b002d27da48eecd9492f
sadberto <82291171+sadberto@users.noreply.github.com>
2022-01-06 07:53:08 +0100

    +BACKUP

--- assets/main.js
+++ assets/main.js
@@ -42,88 +42,70 @@ function hoverProblems(){
     let iconSelectTrust = document.querySelector('.ics_trust');
     let iconSelectPaper = document.querySelector('.ics_paper');
     let iconsSelectEmail = document.querySelector('.ics_email');
+
     let sets = 0;
     addEventListener('click' , e => {
         const el = e.target ;
-        
-                if (el.classList.contains('ics_risk') && sets === 0 ){
+                    if (el.classList.contains('ics_risk')){
                     icsRisk.classList.toggle('trusts');
-                    iconSelectd.setAttribute('src' , 'img/minus.png')
-                    sets ++;
-                    console.log(sets)
+                    console.log('Cheguei aqui');
                 }else {
                 if (el.classList.contains('ics_risk' || sets >= 1)) {
-                    console.log('estou aqui')
                     icsRisk.classList.remove('trusts');
-                    iconSelectd.setAttribute('src' , 'img/plus.png')
-                    console.log(sets)
-                    sets = 0;
                 }
             }
                 if (el.classList.contains('ics_cost') && sets === 0 ){
                     icsCost.classList.toggle('trusts');
-                    iconSelectCost.setAttribute('src' , 'img/minus.png')
-                    sets ++;
                 }else {
                 if (el.classList.contains('ics_cost' || sets >= 1)) {
                     icsCost.classList.remove('trusts');
-                    iconSelectCost.setAttribute('src' , 'img/plus.png')
-                    console.log(sets)
-                    sets = 0;
+    
                 }
             }
 
                 if (el.classList.contains('ics_transp') && sets === 0 ){
                     icsTransp.classList.toggle('trusts');
-                    iconSelectTransp.setAttribute('src' , 'img/minus.png')
-                    console.log(sets)
-                    sets ++;
                 }else {
                     if (el.classList.contains('ics_transp' || sets >= 1)) {
                         icsTransp.classList.remove('trusts');
-                        iconSelectTransp.setAttribute('src' , 'img/plus.png')
-                        sets = 0;
                     }
                 }
         
                 if (el.classList.contains('ics_trust') && sets === 0 ){
                     icsTrust.classList.toggle('trusts');
-                    iconSelectTrust.setAttribute('src' , 'img/minus.png')
-                    sets ++;
                 }else {
                     if (el.classList.contains('ics_trust' || sets >= 1)) {
                         icsTrust.classList.remove('trusts');
-                        iconSelectTrust.setAttribute('src' , 'img/plus.png')
-                        sets = 0;
+        
                     }
                 }
         
                 if (el.classList.contains('ics_paper') && sets === 0 ){
                     icsPaper.classList.toggle('trusts');
-                    iconSelectPaper.setAttribute('src' , 'img/minus.png')
-                    sets ++;
                 }else {
                     if (el.classList.contains('ics_paper' || sets >= 1)) {
                         icsPaper.classList.remove('trusts');
-                        iconSelectPaper.setAttribute('src' , 'img/plus.png')
-                        sets = 0;
                     }
                 }
        
                 if (el.classList.contains('ics_email') && sets === 0 ){
                     icsEmail.classList.toggle('trusts');
-                    iconsSelectEmail.setAttribute('src' , 'img/minus.png')
-                    sets ++;
                 }else {
                     if (el.classList.contains('ics_email' || sets >= 1)) {
                         icsEmail.classList.remove('trusts');
-                        iconsSelectEmail.setAttribute('src' , 'img/plus.png')
-                        sets = 0;
                     }
                 }
+         
+    })
 
-                
-          
+    addEventListener('mouseover' , e => {
+        const el = e.target ;
+        if(el.classList.contains('img-plus')){
+            iconSelectd.classList.toggle('img-kick');
+        }else {
+            iconSelectd.classList.remove('img-kick')
+        }
+            
     })
 }
 
